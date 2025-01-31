c492f159cf24d7ea27ba8a83e1464cb58b2bb777
Martin Rippin <marrip@ihelse.net>
2025-01-31 14:26:48 +0000

    feat: add globbing for ambiguous OUS/HUS paths

--- pronto/pronto.py
+++ pronto/pronto.py
@@ -1,3 +1,8 @@
+import glob
+import logging
+import os
+
+# get tumor mutational burden label
 def get_tmb_string(val):
 	level = "Not available"
 	if(0 <= val <= 5):
@@ -7,3 +12,13 @@ def get_tmb_string(val):
 	elif(val > 20):
 		level = "høy"
 	return "{} mut/Mb; {}\n".format(val, level)
+
+# use glob to find file in different folder structures
+def glob_sample_file(*path_units):
+	glob_string = os.path.join(*path_units)
+	files = glob.glob(glob_string, recursive=True)
+	if len(files) == 1:
+		return files[0]
+	else:
+		logging.error("unsuccessful glob: {}".format(glob_string))
+		raise ValueError
