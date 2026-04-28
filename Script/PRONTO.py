10afde28b8d90df90c599a9f05cf0879f4dd44df
Xiaoli Zhang <81294502+xiaoliz0@users.noreply.github.com>
2026-04-28 12:29:05 +0200

    Update Script/PRONTO.py
    Co-authored-by: Martin Rippin <74295098+marrip@users.noreply.github.com>

--- Script/PRONTO.py
+++ Script/PRONTO.py
@@ -940,6 +940,22 @@ def update_ppt_variant_summary_table(data_nrows,DNA_sampleID,RNA_sampleID,TMB_DR
 			tf5.paragraphs[0].font.size = Pt(7)
 			tf5.paragraphs[0].font.bold = True
 			tf5.paragraphs[0].alignment = PP_ALIGN.CENTER
+		if(TMB_TSO500 != "" and TMB_TSO500 != "NA"):
+			if(TMB_TSO500_nr >= 0 and TMB_TSO500_nr <= 5):
+				roundshape = slide.shapes.add_shape(MSO_SHAPE.OVAL, Cm(7.07), Cm(3.90), Cm(0.58), Cm(0.58))
+				textbox5 = slide.shapes.add_textbox(Inches(2.74), Inches(1.54), Inches(0.32), Inches(0.21))
+			if(TMB_TSO500_nr > 5 and TMB_TSO500_nr <= 20):
+				roundshape = slide.shapes.add_shape(MSO_SHAPE.OVAL, Cm(8.27), Cm(3.90), Cm(0.58), Cm(0.58))
+				textbox5 = slide.shapes.add_textbox(Cm(8.23), Cm(3.90), Cm(0.58), Cm(0.60))
+			if(TMB_TSO500_nr > 20 and TMB_TSO500 != "" and TMB_TSO500 != "NA"):
+				roundshape = slide.shapes.add_shape(MSO_SHAPE.OVAL, Cm(10.26), Cm(3.90), Cm(0.58), Cm(0.58))
+				textbox5 = slide.shapes.add_textbox(Cm(10.20), Cm(3.95), Cm(0.58), Cm(0.60))
+			roundshape.line.color.rgb = RGBColor(255,165,0)
+			tf5 = textbox5.text_frame
+			tf5.paragraphs[0].text = str(TMB_TSO500_nr)
+			tf5.paragraphs[0].font.size = Pt(7)
+			tf5.paragraphs[0].font.bold = True
+			tf5.paragraphs[0].alignment = PP_ALIGN.CENTER
 			tf5.paragraphs[0].font.color.rgb = RGBColor(250,250,250)
 		textbox6 = slide.shapes.add_textbox(Inches(6.23), Inches(1.06), Inches(0.97), Inches(0.19))
 		tf6 = textbox6.text_frame
